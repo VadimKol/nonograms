ef39b840fd353b0e1f99f1f4c545362e7def5819
Vadim Kolymbet <3t0n@inbox.ru>
2024-02-01 17:50:08 +0300

    feat: change directory structure

--- nonograms/src/index.js
+++
@@ -1,4 +0,0 @@
-import './style.scss';
-import './media.scss';
-import './bodybuilder';
-import './nonogram';
---
+++ nonograms/src/js/index.js
@@ -0,0 +1,4 @@
+import '../style/style.scss';
+import '../style/media.scss';
+import './bodybuilder';
+import './nonogram';
